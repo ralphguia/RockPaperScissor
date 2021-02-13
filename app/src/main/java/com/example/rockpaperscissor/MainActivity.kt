d1bf43dd0858ed25d93a2bd395be0769124493fb
Ralph G <->
2021-02-13 12:20:24 +0100

    Activity converted into a fragment of the title screen

--- app/src/main/java/com/example/rockpaperscissor/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissor/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.rockpaperscissor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.rockpaperscissor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
---
+++ app/src/main/java/com/example/rockpaperscissor/TitleFragment.kt
@@ -0,0 +1,25 @@
+package com.example.rockpaperscissor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.rockpaperscissor.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentTitleBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
